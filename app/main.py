a885489b420d7123aea54886f3aebb918b1b9ab8
amet_jimenez <ametjimenezjg@unimagdalena.edu.co>
2024-06-09 10:33:06 -0500

    fix: delete main.py, only use app.py

--- app/main.py
+++
@@ -1,7 +0,0 @@
-# app/main.py
-from fastapi import FastAPI
-from app.auth.auth_router import router as auth_router
-
-app = FastAPI()
-
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
