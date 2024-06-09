ef399e13cadcfa1457a6ee4aeb75a5def17c2068
amet_jimenez <ametjimenezjg@unimagdalena.edu.co>
2024-06-09 10:25:00 -0500

    Update: add news schemas

--- app/schemas/users_schemas.py
+++ app/schemas/users_schemas.py
@@ -6,3 +6,18 @@ class User(BaseModel):
     email : str
     password : str
     status : Optional[int] = 1
+
+class UserCreate(BaseModel):
+    email : str
+    password : str
+
+class UserId(BaseModel):
+    id : int
+
+class UserUpdate(BaseModel):
+    id : int
+    email : Optional[str] = None
+    password : Optional[str] = None
+
+class UserEmail(BaseModel):
+    email : str
