128a2c71079f62200081d8db24fdfd1c2c23893b
amet_jimenez <ametjimenezjg@unimagdalena.edu.co>
2024-06-09 10:25:06 -0500

    Update: add batch functions for create, update and delete users

--- app/routes/batch_routes.py
+++ app/routes/batch_routes.py
@@ -3,36 +3,20 @@ from fastapi import APIRouter, Depends, BackgroundTasks, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from app.database.database import get_db
-from app.schemas.users_schemas import User
+from app.schemas.users_schemas import User, UserCreate, UserUpdate, UserId
 from app.models.users_models import users
 from app.utils.security import hash_password
 from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 import time
+from app.routes.users_router import create_user, get_user, update_user, delete_user
 
 router = APIRouter()
 
-def insert_user(user_data: User, db: Session):
-    try:
-        new_user = {
-            "email": user_data.email,
-            "password": hash_password.hash(user_data.password),
-            "status": user_data.status
-        }
-        result = db.execute(
-            users.insert().values(new_user)
-        )
-        db.commit()
-        return result
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(e))
-
-
-def process_users_batch(users_data: List[User], db: Session):
+def process_users_batch(users_data: List, process_func, db: Session):
     try:
         start_time = time.time()
         for user_data in users_data:
-            insert_user(user_data, db)
+            process_func(user_data, db)
             if time.time() - start_time > 10:
                 print("Batch processing took longer than 10 seconds")
                 break
@@ -43,6 +27,16 @@ def process_users_batch(users_data: List[User], db: Session):
 
 
 @router.post("/webhook/create_users_batch")
-async def webhook_create_users_batch(users_data: List[User], background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
-    background_tasks.add_task(process_users_batch, users_data, db)
+async def webhook_create_users_batch(users_data: List[UserCreate], backgroud_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    backgroud_tasks.add_task(process_users_batch, users_data, create_user, db)
+    return {"message": "Batch processing initiated"}
+
+@router.put("/webhook/update_users_batch")
+async def webhook_update_users_batch(users_data: List[UserUpdate], backgroud_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    backgroud_tasks.add_task(process_users_batch, users_data, update_user, db)
+    return {"message": "Batch processing initiated"}
+
+@router.put("/webhook/delete_users_batch")
+async def webhook_delete_users_batch(users_data: List[UserId], backgroud_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    backgroud_tasks.add_task(process_users_batch, users_data, delete_user, db)
     return {"message": "Batch processing initiated"}
